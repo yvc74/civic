5b7bb670307417d805fffe48143071de058343cb
Chris Weaver <cmagweaver@gmail.com>
2018-05-06 12:13:51 -0700

    Update to use civicVictory Theme

--- packages/component-library/src/Scatterplot/Scatterplot.js
+++ packages/component-library/src/Scatterplot/Scatterplot.js
@@ -1,42 +1,64 @@
 import React, { PropTypes } from 'react';
-import { VictoryChart, VictoryScatter, VictoryTheme, VictoryLabel } from 'victory';
+import {
+  VictoryChart,
+  VictoryScatter,
+  VictoryLabel,
+  VictoryAxis,
+} from 'victory';
 
-// const Scatterplot = ({ data }) => (
-//   <ScatterChart width={400} height={400} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-//     <XAxis dataKey={'x'} name="stature" unit="cm" />
-//     <YAxis dataKey={'y'} name="weight" unit="kg" />
-//     <Scatter name="A school" data={data} fill="#8884d8" />
-//     <CartesianGrid />
-//     <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-//   </ScatterChart>
-// );
+import CivicVictoryTheme from '../VictoryTheme/VictoryThemeIndex';
+import { css } from 'emotion';
 
-const Scatterplot = ({ title, description, data }) => (
-  <VictoryChart>
-    <VictoryLabel
-      textAnchor="middle"
-      text={title}
-      x={200}
-      y={10}
-    />
-    <VictoryLabel
-      textAnchor="middle"
-      text={description}
-      x={200}
-      y={30}
-    />
-    <VictoryScatter
-      style={{ data: { fill: '#c43a31' } }}
-      size={7}
-      data={data}
-    />
-  </VictoryChart>
+const barchartWrapper = css`
+  margin: 0 auto;
+  max-width: 900px;
+  width: 100%;
+`;
+
+const titleStyle = css`
+  font-family: 'filson-soft', sans-serif;
+  font-size: 40px;
+  font-weight: 500;
+  text-align: center;
+  margin: 40px 0 12px 0;
+`;
+
+const subtitleStyle = css`
+  display: block;
+  font-family: 'filson-soft', sans-serif;
+  font-size: 18px;
+  text-align: center;
+`;
+
+const Scatterplot = ({ title, subtitle, data, xLabel, yLabel }) => (
+  <div>
+    { title ? <h3 className={titleStyle}>{title}</h3> : null}
+    { subtitle ? <span className={subtitleStyle}>{subtitle}</span> : null}
+    <div className={barchartWrapper}>
+      <VictoryChart
+        domainPadding={20}
+        animate={{ duration: 300 }}
+        theme={CivicVictoryTheme.civic}
+      >
+        <VictoryLabel text={yLabel} textAnchor="middle" x={50} y={30} />
+        <VictoryLabel text={xLabel} textAnchor="end" x={600} y={285} />
+
+        <VictoryScatter
+          style={{ data: { fill: '#c43a31' } }}
+          size={7}
+          data={data}
+        />
+      </VictoryChart>
+    </div>
+  </div>
 );
 
 Scatterplot.propTypes = {
   title: PropTypes.string,
-  description: PropTypes.string,
+  subtitle: PropTypes.string,
   data: PropTypes.Array,
+  xLabel: PropTypes.string,
+  yLabel: PropTypes.string,
 };
 
 export default Scatterplot;
--- packages/component-library/stories/Scatterplot.story.js
+++ packages/component-library/stories/Scatterplot.story.js
@@ -5,36 +5,52 @@ import { withKnobs, text, object } from '@storybook/addon-knobs/react';
 
 import { Scatterplot } from '../src';
 
-const containerStyle = {
-  display: 'flex',
-  justifyContent: 'center',
-  marginTop: '20px',
-  height: '400px',
-  width: '400px',
-};
 
-const testData = [
-  { x: 1, y: 2 },
-  { x: 2, y: 3 },
-  { x: 3, y: 5 },
-  { x: 4, y: 4 },
-  { x: 5, y: 7 },
+const singleGroup = [
+  { x: 6, y: 6 },
+  { x: 7, y: 4 },
+  { x: 8, y: 3 },
+  { x: 9, y: 6 },
+  { x: 9, y: 8 },
+  { x: 0, y: 3 },
+  { x: 0, y: 2 },
+  { x: 5, y: 3 },
+  { x: 1, y: 5 },
+  { x: 8, y: 4 },
 ];
 
 export default () => storiesOf('Scatterplot')
   .addDecorator(withKnobs)
   .add('Simple usage', () => {
-    const title = text('Title', 'Scatter Plot Title');
-    const description = text('Description', 'Scatterplot description');
-    const data = object('Data', testData);
+    const title = text('Title', 'Concert Attendees and Revenue');
+    const description = text('Description', 'For concerts at XYZ Venus in 2017');
+    const data = object('Data', singleGroup);
 
     return (
-      <div style={{ width: '100%' }}>
-        <div style={containerStyle}>
+      <div>
+        <div>
           <Scatterplot
             title={title}
-            description={description}
+            subtitle={description}
             data={data}
+            xLabel="Attendies"
+            yLabel="Revenue"
+          />
+        </div>
+      </div>
+    );
+  })
+  .add('Multiple Groups', () => {
+    const title = text('Title', 'Concert Attendees and Revenue');
+    const description = text('Description', 'For concerts at XYZ Venus in 2017');
+
+    return (
+      <div>
+        <div>
+          <Scatterplot
+            title={title}
+            subtitle={description}
+            data={singleGroup}
           />
         </div>
       </div>
