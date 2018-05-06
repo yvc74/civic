a8f94018abf922902dd802bb09e6692278e95de4
Chris Weaver <cmagweaver@gmail.com>
2018-05-05 18:09:01 -0700

    Add victory chart dependency and basic scatterplot

--- packages/component-library/src/Scatterplot/Scatterplot.js
+++ packages/component-library/src/Scatterplot/Scatterplot.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import React, { PropTypes } from 'react';
+import { VictoryChart, VictoryScatter, VictoryTheme, VictoryLabel } from 'victory';
 
-const Scatterplot = ({ data }) => (
-  <ScatterChart width={400} height={400} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-    <XAxis dataKey={'x'} name="stature" unit="cm" />
-    <YAxis dataKey={'y'} name="weight" unit="kg" />
-    <Scatter name="A school" data={data} fill="#8884d8" />
-    <CartesianGrid />
-    <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-  </ScatterChart>
+// const Scatterplot = ({ data }) => (
+//   <ScatterChart width={400} height={400} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+//     <XAxis dataKey={'x'} name="stature" unit="cm" />
+//     <YAxis dataKey={'y'} name="weight" unit="kg" />
+//     <Scatter name="A school" data={data} fill="#8884d8" />
+//     <CartesianGrid />
+//     <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+//   </ScatterChart>
+// );
+
+const Scatterplot = ({ title, description, data }) => (
+  <VictoryChart>
+    <VictoryLabel
+      textAnchor="middle"
+      text={title}
+      x={200}
+      y={10}
+    />
+    <VictoryLabel
+      textAnchor="middle"
+      text={description}
+      x={200}
+      y={30}
+    />
+    <VictoryScatter
+      style={{ data: { fill: '#c43a31' } }}
+      size={7}
+      data={data}
+    />
+  </VictoryChart>
 );
 
 Scatterplot.propTypes = {
-  data: React.PropTypes.Array,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  data: PropTypes.Array,
 };
 
 export default Scatterplot;
--- packages/component-library/stories/Scatterplot.story.js
+++ packages/component-library/stories/Scatterplot.story.js
@@ -1,23 +1,42 @@
 import React from 'react';
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text, object } from '@storybook/addon-knobs/react';
+
 import { Scatterplot } from '../src';
 
 const containerStyle = {
   display: 'flex',
   justifyContent: 'center',
   marginTop: '20px',
+  height: '400px',
+  width: '400px',
 };
 
-const data = [{ x: 100, y: 200, z: 200 }, { x: 120, y: 100, z: 260 },
-  { x: 170, y: 300, z: 400 }, { x: 140, y: 250, z: 280 },
-  { x: 150, y: 400, z: 500 }, { x: 110, y: 280, z: 200 }];
+const testData = [
+  { x: 1, y: 2 },
+  { x: 2, y: 3 },
+  { x: 3, y: 5 },
+  { x: 4, y: 4 },
+  { x: 5, y: 7 },
+];
 
 export default () => storiesOf('Scatterplot')
+  .addDecorator(withKnobs)
   .add('Simple usage', () => {
+    const title = text('Title', 'Scatter Plot Title');
+    const description = text('Description', 'Scatterplot description');
+    const data = object('Data', testData);
+
     return (
-      <div style={containerStyle} >
-        <Scatterplot data={data} />
+      <div style={{ width: '100%' }}>
+        <div style={containerStyle}>
+          <Scatterplot
+            title={title}
+            description={description}
+            data={data}
+          />
+        </div>
       </div>
     );
   });
