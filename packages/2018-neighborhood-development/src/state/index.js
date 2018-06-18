497423233df45af7e34df940efe83618388c4fdc
Jaron Heard <jaronheard@gmail.com>
2018-06-18 02:49:25 -0700

    initial working version

--- packages/2018-neighborhood-development/src/state/index.js
+++ packages/2018-neighborhood-development/src/state/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
+import exploreUrbanCampsiteSweeps from './explore-urban-campsite-sweeps';
+
 export default function createReducer(asyncReducers) {
   return combineReducers({
     routing: routerReducer,
+    exploreUrbanCampsiteSweeps,
     ...asyncReducers,
   });
 }
